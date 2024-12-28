9fca927bfcfc5fe0c22dcb67d1d5932999b492cb
Gsoteldo <gsoteldor@gmail.com>
2024-12-28 17:33:55 +0100

    Cambiados los usuarios de inicio.

--- CMSProject/Models/SeedData.cs
+++ CMSProject/Models/SeedData.cs
@@ -54,8 +54,43 @@ namespace CMSProject.Models
                         Email = "Andres@prueba.com",
                         Password = "abcd",
                         IdRole = 2
-                    }
-                    );
+                    },
+                    new UserTable
+                    {
+                        Name = "admin",
+                        Email = "admin@admin.com",
+                        Password = "ykSM8L/htmos7pIGGjLocbrB6nL80r/oRqvjbh0nTsA=                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ",
+                        IdRole = 1,
+                        Salt = "7v9wlOGynx/6TXSKFzdrPw==",
+                        IsActivated = true
+                    },
+                    new UserTable
+                    {
+                        Name = "cliente",
+                        Email = "cliente@cliente.com",
+                        Password = "rRsfq2+ZguU/UQUAJo2kkmEc2Zid1T47HoArv+xSwek=                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ",
+                        IdRole = 3,
+                        Salt = "4A6nPXhqi3BQhprOfkMlKA==",
+                        IsActivated = true
+                    },
+                    new UserTable
+                    {
+                        Name = "creador",
+                        Email = "creador@creador.com",
+                        Password = "Zje1EgnvyaSaKyZU9KSy+gskRoLJDP59TVchD1AG05U=                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ",
+                        IdRole = 2,
+                        Salt = "ZtSeVmmvdq21/N1PQOqYhg==",
+                        IsActivated = true
+                    },
+                    new UserTable
+                    {
+                        Name = "publisher",
+                        Email = "publisher@publisher.com",
+                        Password = "xLUdz8hi+Koc7cBRBVeyKEQlKeS1AeYooPqTRndd05o=                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ",
+                        IdRole = 4,
+                        Salt = "cTYYV5z4/qYCwEUEl1cYiQ==",
+                        IsActivated = true
+                    });
             }
 
             else if (!context.PublicationTables.Any())
